fb0c1a330d4002ba81899a1e0f75c11ab6e74256
Tutuezin <vinnycamara1@gmail.com>
2022-07-24 19:50:29 -0300

    :sparkles: feat: getRentals

--- src/controller/rentalController.js
+++ src/controller/rentalController.js
@@ -7,7 +7,13 @@ export async function getRentals(req, res) {
 
     const { rows: rentals } = await connection.query(
       `
-      
+      SELECT rentals.*,
+      json_build_object('id', customers.id, 'name', customers.name) AS customer,
+      json_build_object('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game
+      FROM rentals
+      JOIN customers ON rentals."customerId" = customers.id
+      JOIN games ON rentals."gameId" = games.id
+      JOIN categories ON games."categoryId" = categories.id
       `
     );
 
@@ -22,16 +28,18 @@ export async function createReantal(req, res) {
   try {
     const { customerId, gameId, daysRented } = req.body;
     const rentDate = dayjs().format("YYYY-MM-DD");
+    console.log(rentDate);
+
     const { pricePerDay } = res.locals;
 
     const originalPrice = pricePerDay * daysRented;
 
     await connection.query(
       `
-     INSERT INTO rentals
-     ("customerId",
-      "gameId","rentDate", "daysRented", "returnDate", "originalPrice","delayFee")
-     VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+    INSERT INTO rentals
+    ("customerId",
+    "gameId","rentDate", "daysRented", "returnDate", "originalPrice","delayFee")
+    VALUES ($1, $2, $3, $4, $5, $6, $7)`,
       [customerId, gameId, rentDate, daysRented, null, originalPrice, null]
     );
 
